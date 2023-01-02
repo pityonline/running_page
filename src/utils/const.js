5ebdae3e85c89fb701aab46e1d172aa9ab3492bf
pityonline <pityonline@gmail.com>
2023-01-02 08:33:02 +0800

    Update cities
    Signed-off-by: pityonline <pityonline@gmail.com>

--- src/utils/const.js
+++ src/utils/const.js
@@ -3,11 +3,8 @@ const MAPBOX_TOKEN =
   'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
-  '上海市',
-  '天津市',
-  '重庆市',
-  '香港特别行政区',
-  '澳门特别行政区',
+  '深圳市',
+  '沧州市',
 ];
 
 // styling: set to `true` if you want dash-line route
